4219b839f31492595b170b528a0f02f5004760c5
mahdi niki <mahdi.nikgoftar@gmail.com>
2024-11-11 23:43:06 +0330

    s

--- quiz6.js
+++
@@ -1,7 +0,0 @@
-//?Q-4-Consider this array. Remove the colors blue and yellow.
-// Please use variables given to you and Do Not change the name of them!!
-let colors = ["red", "green", "blue", "yellow", "orange"];
-
-let filterdColors = colors.splice(2, 2);
-
-console.log(colors);
--- quiz7.js
+++
@@ -1,7 +0,0 @@
-//?Q-5:Take these two arrays and convert them into one array.
-// Please use variables given to you and Do Not change the name of them!!
-
-let primeNumbers = [2, 3, 5, 7];
-let evenNumbers = [2, 4, 6, 8];
-let oneArray= evenNumbers.concat(primeNumbers);
-console.log(oneArray)
