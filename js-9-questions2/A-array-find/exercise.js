41de9d85fce55a84986e4e95c1cc400c6b5b9f5d
mahdi niki <mahdi.nikgoftar@gmail.com>
2024-11-30 23:48:55 +0330

    add file

---
+++ js-9-questions2/A-array-find/exercise.js
@@ -0,0 +1,27 @@
+/* 
+  You are given an array of names.
+  Write a function called "findLongNameThatStartsWithA" using the .find() method. This function should find the first name in names array that starts with the letter 'A' and is longer than seven letters.
+*/
+
+// write your code here
+let names = [
+  "Vajihe",
+  "Alireza",
+  "AmirMostafa",
+  "AmirMehdi",
+  "Alie",
+  "Sarah",
+  "MohammadHosein",
+  "Fatemeh",
+  "Mahjan",
+];
+
+const findLongNameThatStartsWithA = (arr) =>
+  arr.find((nam) => nam.length > 7 && nam[0] === "A"); // create the function
+
+let longNameThatStartsWithA = findLongNameThatStartsWithA(names); // call the function
+
+console.log(longNameThatStartsWithA);
+
+/* EXPECTED OUTPUT */
+// "AmirMostafa"
